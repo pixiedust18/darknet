705528d6fea440d87124ff3bcb2701020375b983
Krisha Bhambani <62170625+pixiedust18@users.noreply.github.com>
2020-08-07 04:20:39 +0530

    Update demo.c

--- src/demo.c
+++ src/demo.c
@@ -284,7 +284,7 @@ void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int 
                 }
             }
             printf("calling dd\n");
-            if (!benchmark && !dontdraw_bbox){cout<<"calling next"; draw_detections_cv_v3(show_img, local_dets, local_nboxes, demo_thresh, demo_names, demo_alphabet, demo_classes, demo_ext_output);}
+            if (!benchmark && !dontdraw_bbox){printf("calling dd2\n"); draw_detections_cv_v3(show_img, local_dets, local_nboxes, demo_thresh, demo_names, demo_alphabet, demo_classes, demo_ext_output);}
             printf("after dd\n");
             free_detections(local_dets, local_nboxes);
             printf("Where err????");
