784cdc495e6aabea72c0b8a7af68392f30fd2dc0
Krisha Bhambani <krisha.bhambani@gmail.com>
2020-08-08 04:48:11 +0530

    Update darknet_video.py

--- darknet_video.py
+++ darknet_video.py
@@ -25,6 +25,7 @@ def check(p1, p2, w1, w2, h1, h2, SD, f):
     x1, y1 = p1[0], p1[1]
     x2, y2 = p2[0], p2[1]
     if(x1==x2 and y1==y2):
+<<<<<<< HEAD
         return True
     coords = [(x1, y1), (x2, y2)]
        
@@ -34,6 +35,22 @@ def check(p1, p2, w1, w2, h1, h2, SD, f):
     if(social_distance > 0 and social_distance < 0.25 * param):
         return False
     
+=======
+        print("eq")
+        return True
+    v1 = 1.6 * f / (h1)
+    v2 = 1.6 * f / (h2)
+    print("v1, v2", v1, v2)
+    sensor_w, sensor_h = 4.8, 3.6
+    sensor_w_px, sensor_h_px = 3200, 2400
+    real_dist = sensor_w * abs(x1-x2) / sensor_w_px
+    x1_ = 0
+    x2_ = real_dist
+    ed = math.sqrt((x1_ - x2_)*(x1_ - x2_) + (v1-v2) * (v1 - v2))    
+    print(ed)
+    if (ed>0 and ed<SD):
+        return False
+>>>>>>> parent of 03dafd3... Update darknet_video.py
     return True
     '''param = (x1+x2)/2
     if(social_distance > 0 and social_distance < 0.25 * param):
